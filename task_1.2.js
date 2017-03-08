17421b47a397efbfb9950d3064ff176147c4700d
evglaptev <evglaptev23@gmail.com>
2017-03-08 22:54:18 +0300

    upd task 1.2

--- task_1.2.js
+++ task_1.2.js
@@ -1,12 +1,13 @@
-
 function run() {
     for (var i = 0; i < 10; i++) {
-        var t = new Number(i);
-        setTimeout(logWrite(t)
-        , 1000);
+        (function () {
+            var t = i;
+            setTimeout(function () {
+                console.log(t);
+            }, 1000);
+        })();
+
     }
-    function logWrite(t) {
-        console.log(t);
-    }
+
 }
 run();
